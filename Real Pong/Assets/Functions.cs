7433f4882dde36bff0cc1261f1d0b95c36f1064b
OmarGamra <gamraomar03@gmail.com>
2020-06-17 11:35:20 +0100

    realpong

---
+++ Real Pong/Assets/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    public float a, b;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Sum(a,b));
+        Debug.Log(Div(a,b));
+        Debug.Log(Prod(a,b));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    float Sum(float x, float y)
+    {
+        return (x + y);
+    }
+    float Prod(float x, float y)
+    {
+        return (x * y);
+    }
+    float Div(float x, float y)
+    {
+        if (y == 0)
+        {
+            Debug.Log("Error");
+            return 0;
+        }
+        else
+        {
+            return (x / y);
+        }
+    }
+
+
+}
---
+++ Real Pong/Assets/Scenes/Ball.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Ball : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreRightText;
+    public TextMeshProUGUI ScoreLeftText;
+    int ScoreRight;
+    int ScoreLeft;
+    public float speed;
+
+    // Update is called once per frame
+    void Start()
+    {
+        ScoreRight = 0;
+        ScoreLeft = 0;
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+    
+
+    float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "RacketLeft")
+        {
+            float y = HitFactor(transform.position,
+               col.transform.position,
+               col.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+        if (col.gameObject.name == "RacketRight")
+        {
+            float y = HitFactor(transform.position,
+                col.transform.position,
+                col.collider.bounds.size.y);
+            Vector2 dir = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (col.gameObject.name == "WallRight")
+        {
+            ScoreLeft++;
+            ScoreLeftText.text = ScoreLeft.ToString();
+        }
+
+
+        if (col.gameObject.name == "WallLeft")
+        {
+            ScoreRight++;
+            ScoreRightText.text = ScoreRight.ToString();
+        }
+    }
+
+}
